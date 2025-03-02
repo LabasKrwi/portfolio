cab5ccf50ba435f3d77b282b45e0e152ebe83572
Илья Коперман <ilukhankoperman@gmail.com>
2025-03-02 20:41:46 +0300

    0.8.0

--- src/generalstore/effects.ts
+++ src/generalstore/effects.ts
@@ -2,7 +2,7 @@ import { createEffect } from "effector";
 import axios, { AxiosResponse } from "axios";
 import { IProject } from "../types/Project";
 import { ITech } from "../types/Techs";
-const BASE_URL = 'https://database.free.beeceptor.com';
+const BASE_URL = 'https://api.npoint.io/985bfbe8cf021418f80e';
 
 export const fetchProjectsEffect = createEffect( async ()=> {
     const response: AxiosResponse<IProject[]> = await axios.get(`${BASE_URL}/projects`);
@@ -16,10 +16,10 @@ export const fetchTechsEffect = createEffect( async ()=> {
 })
 
 export const fetchProjectByIdEffect = createEffect( async (id: number)=> {
-    const response: AxiosResponse<IProject> = await axios.get(`${BASE_URL}/p${id}`);
+    const response: AxiosResponse<IProject> = await axios.get(`${BASE_URL}/projects/${id-1}`);
     return response;
 })
 export const fetchTechsByIdEffect = createEffect( async (id: number)=> {
-    const response: AxiosResponse<ITech> = await axios.get(`${BASE_URL}/t${id}`);
+    const response: AxiosResponse<ITech> = await axios.get(`${BASE_URL}/techs/${id-1}`);
     return response;
 })
