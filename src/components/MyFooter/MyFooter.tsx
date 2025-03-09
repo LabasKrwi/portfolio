024289a67abf2544c352dfa5482c388dd9bdbbec
Илья Коперман <ilukhankoperman@gmail.com>
2025-03-09 20:51:36 +0300

    0.12.0

--- src/components/MyFooter/MyFooter.tsx
+++ src/components/MyFooter/MyFooter.tsx
@@ -30,8 +30,8 @@ const MyFooter:FC<MyMenuProps> = ({...props}) => {
           <li>ilukhan791@mail.ru</li>
           <li>+7 (916) 670-27-59</li>
           <li><a href="https://hh.ru/applicant/resumes/view?resume=8aac7c42ff0e6d7ebb0039ed1f714d6531614c">Мой HH.ru</a></li>
-          <li><a href="https://drive.google.com/file/d/1EPrb6YxnFRq8DsurkMCX1Bar8SQ4AZBz/view?usp=sharing">PDF Резюме для HR-отдела</a></li>
-          <li><a href="https://drive.google.com/file/d/1GWy7r_qny1_AfroQcxiIFYbAobzHQ7M3/view?usp=sharing">Резюме</a></li>
+          <li><a href="https://drive.google.com/file/d/1N_NGSKwAphuaZyRUJokyLSkqGxLtmFU5/view?usp=sharing">PDF Резюме c HH.ru</a></li>
+          <li><a href="https://drive.google.com/file/d/14a1rRlMKobVtSy2YR75haEV49lPWJI0s/view?usp=sharing">Мое резюме</a></li>
         </ul>
         </div>
       </div>
